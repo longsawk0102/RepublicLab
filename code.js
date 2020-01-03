f95a6ea395b734fbe3ca1a5b458a56e9e4b6f4c3
longsawk0102 <59468839+longsawk0102@users.noreply.github.com>
2020-01-03 14:29:46 -0800

    Update code.js

--- code.js
+++ code.js
@@ -543,7 +543,7 @@ bot.on('message', async message => {
         remainingErrorNumber = "This user is due for a promotion.";
       }
 
-      var remainingError = `**${remainingErrorNumber}** ${xpName} remaining for **${nextRankName} (${requiredXP} ${xpName})**`
+      var remainingError = `${xpName} remaining for **${nextRankName} (${requiredXP} ${xpName})** **${remainingErrorNumber}**`
 
 
       var response = new Discord.RichEmbed()
